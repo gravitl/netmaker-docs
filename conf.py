f527549800c942dea5f02f59c0e11b2b11212360
afeiszli <alex.feiszli@gmail.com>
2022-03-15 17:57:00 -0400

    changing version to 0.12.0

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Alex Feiszli'
 author = 'Alex Feiszli'
 
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
+release = '0.12.0'
 
 
 # -- General configuration ---------------------------------------------------
