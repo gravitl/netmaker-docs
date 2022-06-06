0fbf90d46e40870aa554cac1a31d897ca9e0fa42
afeiszli <alex.feiszli@gmail.com>
2022-06-06 12:05:51 -0400

    adding versions

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Alex Feiszli'
 author = 'Alex Feiszli'
 
 # The full version, including alpha/beta/rc tags
-release = '0.14.0'
+release = '0.14.2'
 
 
 # -- General configuration ---------------------------------------------------
