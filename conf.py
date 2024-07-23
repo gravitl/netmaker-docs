e957ffd213c31dec92e13081083f036de41dd87f
NEETweeb <dentadlp@gmail.com>
2024-07-23 21:52:47 +0800

    test-customization

--- conf.py
+++ conf.py
@@ -52,6 +52,15 @@ html_theme = 'renku'
 html_show_sourcelink = False
 
 html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
     'nav_title': 'Netmaker Docs',
     'repo_url': 'https://github.com/gravitl/netmaker/',
     "repo_name": "Netmaker",
