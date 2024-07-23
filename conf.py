991e22bfe946040d2679877dbf89f3167613557f
NEETweeb <dentadlp@gmail.com>
2024-07-23 23:05:34 +0800

    added-teal-logo-changed-section-menu-font-color

--- conf.py
+++ conf.py
@@ -80,7 +80,7 @@ html_sidebars = {
         "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
 }
 
-html_logo = 'images/netmaker.png'
+html_logo = 'images/netmaker-teal.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
