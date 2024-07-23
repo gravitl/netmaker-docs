996a2e475af57352e1175c61808dcf46ba57fc3d
NEETweeb <dentadlp@gmail.com>
2024-07-23 21:55:19 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
     'nav_title': 'Netmaker Docs',
     'repo_url': 'https://github.com/gravitl/netmaker/',
     "repo_name": "Netmaker",
